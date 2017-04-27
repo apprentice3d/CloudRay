466c0a7312ba44cad5cabf2e733ba0bca2065e48
Denis Grigor <apprentice3d@gmail.com>
2017-04-26 20:59:26 -0400

    Fixed the encoding.

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	//"log"
+	//"net/http"
 )
 
 func main() {
 
 	fmt.Println("Started server on port 3009 ...")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Server is up!"))
-	})
-
-	log.Fatal(http.ListenAndServe(":3009", nil))
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	w.Write([]byte("Server is up!"))
+	//})
+	//
+	//log.Fatal(http.ListenAndServe(":3009", nil))
 
 }
